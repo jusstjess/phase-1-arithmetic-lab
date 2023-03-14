13da709fa553a6a467c471bd214932976e2654b5
Jess Leyva <jessica.leyvat@yahoo.com>
2023-03-13 18:53:51 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 =31;
+const num2 =2;
+const multiply = (num1 * num2);
+
+
+let random = Math.ceil(Math.random());
+
+const num3 = 25;
+const num4 = 7;
+let mod = (num3 % num4);
+
+
+
+const max = Math.max(2,10,16,20);
+
